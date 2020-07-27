2df5c6bed33c822a42d9d2d9a125388870b9790e
joshualu <joshlu9@gmail.com>
2020-07-27 01:41:06 -0700

    consolidate data and split

--- dimension_decomposition.py
+++ dimension_decomposition.py
@@ -1,17 +1,40 @@
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from matplotlib import pyplot as plt
+
 
 import pandas as pd
 
 
-df = pd.read_csv('tracks.csv')
-print(df)
+df = pd.read_csv('tracks.csv')                  #read tracks
 
-features = ['title','danceability', 'energy', 'loudness', 'speechiness', 'acousticness',
-              'liveness', 'valence']
+features = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness',
+              'liveness', 'valence']            #declare variables that we will set for "x"
+            
+x = df.loc[:, features].values
 
-x = df.loc['danceability', 'energy', 'loudness', 'speechiness', 'acousticness',
-              'liveness', 'valence']
+y = df.loc[:,['title']].values                  #make y the title variable of each song 
+
+
+x = StandardScaler().fit_transform(x)           #standardize the data, by scaling the values
 print(x)
-y = df.loc[:,['title']].values
-print(y)
+
+
+
+
+## Now we can test multiple different dimensionality reduction techniques
+
+#usinf PCA first
+pca = PCA(n_components=2)
+print(pca)
+
+principalComponents = pca.fit_transform(x)
+
+principalDf = pd.DataFrame(data = principalComponents
+             , columns = ['component 1', 'component 2'])
+
+print(principalDf)
+
+
+
